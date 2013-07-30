8062d1a4f5d419daa7b6d95d1d9a215bf6083e23
mezhevikin <mezhevikin@gmail.com>
2013-07-30 14:44:25 +0400

    Initial Commit

---
+++ MCNetwork/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MCNetwork
+//
+//  Created by Администратор on 7/30/13.
+//  Copyright (c) 2013 Администратор. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MCNetwork/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MCNetwork
+//
+//  Created by Администратор on 7/30/13.
+//  Copyright (c) 2013 Администратор. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
